347382145cfbbf7bed72e18878453df302ce0e55
Chris Cardle <cdcardle@gmail.com>
2020-01-07 15:30:56 -0500

    added sessions/new view and adjusted controller/routes for it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,10 +10,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    if @user && @user.authenticate(params[:user][:password])
+    @user = User.find_by(email: params[:sessions][:email])
+    if @user && @user.authenticate(params[:sessions][:password])
       session[:user_id] = @user.id
-      redirect_to user_path(@user), notice: "Welcome back to the theme park!"
+      redirect_to user_path(@user), notice: "Welcome back, #{@user.name}!"
     else
       redirect_to signin_path
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get "/signin", to: "sessions#new"
+  post "/sessions/create", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
   
   get '/home', to: "static#home"
 
