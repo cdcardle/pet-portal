5bba307af91f2b094e850575050fdbf220fb1ffe
Chris Cardle <cdcardle@gmail.com>
2020-01-21 16:17:51 -0500

    added pets#show tests

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe PetsController, type: :controller do
   include_examples "create models"
 
   describe "#index" do
-    it "renders index for admin" do
+    it "renders for admin" do
       sign_in admin
       expect(get :index).to render_template(:index)
     end
@@ -18,14 +18,27 @@ RSpec.describe PetsController, type: :controller do
   end
 
   describe "#new" do
-    it "renders new if admin" do
+    it "renders if admin" do
       sign_in admin
       expect(get :new).to render_template(:new)
     end
 
     it "redirects back if not admin" do
       expect(
-        get :index, params: {headers: {"HTTP_REFERER" => "http://test.host"}}
+        get :new, params: {headers: {"HTTP_REFERER" => "http://test.host"}}
+      ).to redirect_to(root_path)
+    end
+  end
+
+  describe "#show" do
+    it "renders if admin" do
+      sign_in admin
+      expect(get :show).to render_template(:show)
+    end
+
+    it "redirects back if not admin" do
+      expect(
+        get :show, params: {headers: {"HTTP_REFERER" => "http://test.host"}}
       ).to redirect_to(root_path)
     end
   end
