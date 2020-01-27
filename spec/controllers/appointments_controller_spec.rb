02b6cfcd8a40f3862dbb9cad9bdb1088943215f4
Chris Cardle <cdcardle@gmail.com>
2020-01-27 10:20:31 -0500

    wrote tests for appointments#destroy

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -62,13 +62,13 @@ RSpec.describe AppointmentsController, type: :controller do
     include_examples "redirects to sign in if not logged in", :show, { id: 1 }
   end
 
-  describe "edit" do
+  describe "#edit" do
     include_examples "renders if admin", :edit, {id: 1}
     include_examples "redirects back if user", :edit, {id: 1}
     include_examples "redirects to sign in if not logged in", :edit, {id: 1}
   end
 
-  describe "update" do
+  describe "#update" do
     it "updates the appointment and redirects to its show page if admin" do
       sign_in admin
       post :update, params: {id: 1, appointment: {datetime: "2020-01-09T11:30:00"}}
@@ -79,5 +79,29 @@ RSpec.describe AppointmentsController, type: :controller do
 
     include_examples "redirects back if user", :update, {id: 1, appointment: {datetime: "2020-01-09T11:30:00"}}
     include_examples "redirects to sign in if not logged in", :update, {id: 1, appointment: {datetime: "2020-01-09T11:30:00"}}
+  end
+
+  describe "#destroy" do
+    it "deletes an appointment and redirects to appointments#index if admin" do
+      sign_in admin
+      post :create, params: {appointment: appointment_params}
+      expect {
+        delete :destroy, params: {id: Appointment.last.id}
+      }.to change(Appointment, :count).by(-1)
+      expect(response).to redirect_to(appointments_path)
+    end
+
+    it "does nothing if user" do
+      sign_in user
+      expect {
+        delete :destroy, params: {id: Appointment.last.id}
+      }.to_not change(Appointment, :count)
+    end
+
+    it "does nothing if not logged in" do
+      expect {
+        delete :destroy, params: {id: Appointment.last.id}
+      }.to_not change(Appointment, :count)
+    end
   end
 end
